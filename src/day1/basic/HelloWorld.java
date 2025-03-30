9d144ff276be3490d0ba44905fd4b0cc9627caf6
Shashangka <shashank8848ktm@gmail.com>
2025-03-30 15:50:42 -0500

    first commit

---
+++ src/day1/basic/HelloWorld.java
@@ -0,0 +1,10 @@
+package day1.basic;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // sout shortcut used then tab is used
+        System.out.println("Hello, World shashangka is nepali hero ");
+
+
+    }
+}
---
+++ src/day1/basic/Introduction.java
@@ -0,0 +1,12 @@
+package day1.basic;
+
+public class Introduction   {
+    //every 6 months java new version is installed.
+    //james gosling created it.
+
+    public static void main(String[] args) {
+        System.out.println("Hello I am shashangka");
+        System.out.println("Hello I am software engineer" );
+        System.out.println("Hello,I will teach you java for 2 months\n After 2months,you will become Java developer");
+    }
+}
