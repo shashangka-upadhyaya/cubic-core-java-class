2776771b470c5e79076381cbab644b398e742874
Shashangka <shashank8848ktm@gmail.com>
2025-04-14 18:09:01 -0500

    my assignments to help my readers java easy

---
+++ src/day9/oops/CommentsDemo.java
@@ -0,0 +1,12 @@
+package day2.basic;
+
+public class CommentsDemo {
+    //This is a java program
+    /*  Hello, I am Learning Java.
+    I am beginner.
+    I know, after completing this class
+    I will become Java Developer.
+    */
+
+
+}
